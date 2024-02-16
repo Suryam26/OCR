2e345b7f02efa0b202631035e67eb61fc0187ddf
Psudopodiya <chinmay.raiker2002@gmail.com>
2024-02-16 09:48:19 +0530

    Changes in dropdown css

--- src/components/StateDropdown.tsx
+++ src/components/StateDropdown.tsx
@@ -1,35 +1,41 @@
-import React from 'react'
+import React from 'react';
 
 type StateDropdownProps = {
-  onStateSelected: (selectedState: string) => void;
+    onStateSelected: (selectedState: string) => void;
 };
 
 const StateDropdown: React.FC<StateDropdownProps> = ({ onStateSelected }) => {
-  const [selectedOption, setSelectedOption] = React.useState('');
+    const [selectedOption, setSelectedOption] = React.useState('');
 
-  const handleChange = (event:React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOption(event.target.value);
-    onStateSelected(event.target.value); // Notify the parent component
-  };
- 
-  const options = [
-    { value: 'California', label: 'California' },
-    { value: 'Texas', label: 'Texas' },
-  ];
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOption(event.target.value);
+        onStateSelected(event.target.value); // Notify the parent component
+    };
 
-  return (
-    <div className="mx-auto flex item-center justify-center gap-5 pb-5 ">
-      <select value={selectedOption} onChange={handleChange}>
-        <option value="">Select an option</option>
-        {options.map(option => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-      {selectedOption && <div>You selected: {selectedOption}</div>}
-    </div>
-  );
-}
+    const options = [
+        { value: 'California', label: 'California' },
+        { value: 'Texas', label: 'Texas' },
+    ];
 
-export default StateDropdown
+    return (
+        <div className="item-center mx-auto flex justify-center pb-5 ">
+            <select
+                value={selectedOption}
+                onChange={handleChange}
+                className="mx-auto flex rounded-xl border-8  border-solid
+                        border-[#ECF3FF]
+                        px-2 py-2 
+                        text-sm"
+            >
+                <option value="">Select an option</option>
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default StateDropdown;
